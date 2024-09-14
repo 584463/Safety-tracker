1974e1bfd0dfe2e535dffb642a9d797c58be2268
mstr3sl <584463@eriesd.org>
2024-09-14 01:00:16 -0400

    edit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
-import Login from './Login';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./login.js"; // Ensure this matches the actual file name
 
 function App() {
   return (
